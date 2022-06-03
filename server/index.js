ceaffe7cfdc5702ad7c4df381285387d9acc6e16
Janith Gamage <janithgamage1.ed@gmail.com>
2022-06-03 22:55:22 +0530

    fix: update project structture

--- server/index.js
+++ server/index.js
@@ -5,17 +5,10 @@ import cors from 'cors';
 
 //import routes
 import postRoutes from "./routes/posts.routes.js";
-import StudentRoutes from "./routes/student.routes.js";
-import StudentContactRoutes from "./routes/studentContactNo.routes.js";
 import GroupRoutes from "./routes/group.routes.js";
 import GroupDetailsRoutes from "./routes/groupDetails.routes.js";
 import PresentationTempRoutes from "./routes/presentationTemplate.routes.js";
 import panelRoutes from "./routes/panel.routes.js";
-import panelMemberRoutes from "./routes/panelMember.routes.js";
-import staffRoutes from "./routes/staff.routes.js";
-import staffContactNoRoutes from "./routes/staffContactNo.routes.js";
-import staffEmailRoutes from "./routes/staffEmail.routes.js";
-import adminRoutes from "./routes/admin.routes.js";
 import markingSchemaRoutes from "./routes/markingSchema.routes.js";
 import markingSchemaDescriptionRoutes from "./routes/markingSchemaDescription.routes.js";
 import userRoutes from "./routes/user.routes.js"
@@ -35,17 +28,10 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-app.use('/student', StudentRoutes);
-app.use('/studentCon', StudentContactRoutes);
 app.use('/group', GroupRoutes);
 app.use('/groupDetails', GroupDetailsRoutes);
 app.use('/presentationTemp', PresentationTempRoutes);
 app.use('/panel', panelRoutes);
-app.use('/panelMember', panelMemberRoutes);
-app.use('/staff', staffRoutes);
-app.use('/staffContactNo', staffContactNoRoutes);
-app.use('/staffEmail', staffEmailRoutes);
-app.use('/admin', adminRoutes);
 app.use('/markingSchema', markingSchemaRoutes);
 app.use('/markingSchemaDescription', markingSchemaDescriptionRoutes);
 app.use('/user', userRoutes);
